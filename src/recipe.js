2e6fc4d675d3c3f7d9c95977e89296051d1fa5ac
Jack <jack.branthwaite@gmail.com>
2021-02-24 14:52:25 +1300

    ingredients added

--- src/recipe.js
+++ src/recipe.js
@@ -4,6 +4,11 @@ const Recipe = ({ title, calories, image, ingredients }) => {
   return (
     <div>
       <h1>{title}</h1>
+      <ol>
+        {ingredients.map((ingredient) => (
+          <li>{ingredient.text}</li>
+        ))}
+      </ol>
       <p>{calories}</p>
       <img src={image} alt="" />
     </div>
