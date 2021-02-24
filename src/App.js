a5f33bd9f102627e7eee6870c5b97a7177f0fe65
Jack <jack.branthwaite@gmail.com>
2021-02-24 14:34:20 +1300

    pulling from the API database correctly

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import React, { useEffect, useState } from "react";
+import Recipe from "./recipe";
 
 const App = () => {
   const APP_ID = "715ed0ed";
@@ -15,16 +16,27 @@ const App = () => {
     const response = await fetch(
       `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
     );
-    const data = response.json();
+    const data = await response.json();
     setRecipes(data.hits);
+    console.log(data.hits);
   };
 
   return (
     <div className="App">
       <form className="search-form">
         <input className="search-bar" type="text" />
-        <button className="search-button" type="submit"></button>
+        <button className="search-button" type="submit">
+          Search
+        </button>
       </form>
+      {recipes.map((recipe) => (
+        <Recipe
+          key={recipe.recipe.label}
+          title={recipe.recipe.label}
+          calories={recipe.recipe.calories}
+          image={recipe.recipe.image}
+        />
+      ))}
     </div>
   );
 };
